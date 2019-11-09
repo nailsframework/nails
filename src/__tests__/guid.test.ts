a771c008c060cea468e857eca0638012b880ac06
Dominic Järmann <dominic.jaermann@icloud.com>
2019-11-09 23:13:30 +0100

    1.0.5

--- src/__tests__/guid.test.ts
+++ src/__tests__/guid.test.ts
@@ -1,7 +1,7 @@
 import { Guid } from '../core/math/Guid';
 
 test('Guid generation', () => {
-    const first = Guid.newGuid();
-    const second = Guid.newGuid();
-    expect(first).not.toEqual(second);
+  const first = Guid.newGuid();
+  const second = Guid.newGuid();
+  expect(first).not.toEqual(second);
 });
