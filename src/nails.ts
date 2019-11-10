73a602fc7834c59aeb611e533527a874d73d3ea1
Dominic Järmann <dominic.jaermann@icloud.com>
2019-11-10 01:36:17 +0100

    ie

--- src/nails.ts
+++ src/nails.ts
@@ -60,7 +60,7 @@ export class Nails {
     this.engine.indexDOM();
     this.componentEngine.renderComponents();
     this.engine.setTitle();
-    this.state.methods.getState = function() {
+    this.state.methods.getState = function () {
       return this.state;
     };
     if (typeof this.state.methods.onMounted !== 'undefined') {
@@ -93,7 +93,7 @@ export class Nails {
   }
 
   public setUpProxy() {
-    if ('Proxy' in window) {
+    if (typeof window.Proxy !== 'undefined') {
       const handler = {
         state: this.state,
         // tslint:disable-next-line: object-literal-sort-keys
