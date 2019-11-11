0218acac9ef5597747bbfd39c7ae74072faa35b0
Dominic Järmann <dominic.jaermann@icloud.com>
2019-11-11 17:38:33 +0100

    useState hook

--- src/core/components/core.component.ts
+++ src/core/components/core.component.ts
@@ -4,34 +4,34 @@ import { RenderingEngine } from '../engine/engine';
 import { State } from '../state';
 
 export class CoreComponent implements IComponent {
-  public selector: string = 'component';
-  constructor(public state: State) {}
+    public selector: string = 'component';
+    constructor(public state: State) { }
 
-  public render() {
-    /* html */
-    return `<div></div>`;
-  }
-  public useState() {
-    const renderingEngine = new RenderingEngine(this.state);
-    if (typeof window.Proxy !== 'undefined') {
-      const handler = {
-        state: this,
-        // tslint:disable-next-line: object-literal-sort-keys
-        notifyDom: renderingEngine.notifyDOM,
-        engine: renderingEngine,
-
-        get(target: any, prop: any, receiver: any) {
-          return target[prop];
-        },
-        set(target: any, prop: any, value: string) {
-          target[prop] = value;
-          this.notifyDom(target, prop, '');
-          return true;
-        },
-      };
-
-      const proxy = new Proxy(this, handler);
-      this.state.data = proxy;
+    public render() {
+        /* html */
+        return `<div></div>`;
     }
-  }
+    public useState() {
+        const renderingEngine = new RenderingEngine(this.state);
+        if (typeof window.Proxy !== 'undefined') {
+            const handler = {
+                state: this,
+                // tslint:disable-next-line: object-literal-sort-keys
+                notifyDom: renderingEngine.notifyDOM,
+                engine: renderingEngine,
+
+                get(target: any, prop: any, receiver: any) {
+                    return target[prop];
+                },
+                set(target: any, prop: any, value: string) {
+                    target[prop] = value;
+                    this.notifyDom(target, prop, '');
+                    return true;
+                },
+            };
+
+            const proxy = new Proxy(this, handler);
+            this.state.data = proxy;
+        }
+    }
 }
