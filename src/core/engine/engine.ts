02a5ec099b2dcf50242b85b760b3a5c8f91a02e7
Dominic Järmann <dominic.jaermann@icloud.com>
2019-11-12 10:41:51 +0100

    1.0.53

--- src/core/engine/engine.ts
+++ src/core/engine/engine.ts
@@ -170,8 +170,8 @@ export class RenderingEngine {
         // tslint:disable-next-line:no-eval
         eval(
           'this.directives.' +
-          directive +
-          '(element, this.getElementAttributeForDirective(element, directive), this.state)',
+            directive +
+            '(element, this.getElementAttributeForDirective(element, directive), this.state)',
         );
         const nDirectives = this.getElementDirectives(element);
         if (add) {
@@ -292,7 +292,7 @@ export class RenderingEngine {
       console.log('instance.getComponent().' + interpolation);
       // tslint:disable-next-line:no-console
       // tslint:disable-next-line: no-eval
-      console.log(eval('instance.getComponent().' + interpolation))
+      console.log(eval('instance.getComponent().' + interpolation));
       // tslint:disable-next-line:no-eval
       return eval('instance.getComponent().' + interpolation);
     }
@@ -301,7 +301,7 @@ export class RenderingEngine {
   }
 
   // tslint:disable-next-line:no-empty
-  public interpolateOnTextWithState(text: string, state: State) { }
+  public interpolateOnTextWithState(text: string, state: State) {}
   public getContentOfNodeIfTextNodeExists(node: Node): string {
     if (node.nodeType === 3) {
       return node.nodeValue;
