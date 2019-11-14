d3761f9d628ae281d2971e65170175cc5df46574
Dominic Järmann <dominic.jaermann@icloud.com>
2019-11-14 16:12:24 +0100

    1.1.98

--- src/mocks/mock.component.ts
+++ src/mocks/mock.component.ts
@@ -1,13 +1,13 @@
-import { CoreComponent } from "../core/components/core.component";
-import { State } from "../core/state";
+import { CoreComponent } from '../core/components/core.component';
+import { State } from '../core/state';
 
 export class MockComponent extends CoreComponent {
-    constructor(state: State) {
-        super(state);
-        this.selector = "mock";
-    }
+  constructor(state: State) {
+    super(state);
+    this.selector = 'mock';
+  }
 
-    render() {
-        return '<div><p>mock</p></div>'
-    }
+  public render() {
+    return '<div><p>mock</p></div>';
+  }
 }
